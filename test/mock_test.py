e44fb701ee94c03d2e65708966c0186f32059979
Minh Phan <anhminh1993@icloud.com>
2022-06-28 15:49:34 +0700

    debug mock test

--- test/mock_test.py
+++ test/mock_test.py
@@ -17,13 +17,12 @@ from round import Round
 class MockTestMethods(unittest.TestCase):
     # test calls to a method
     def test_player(self):
-        mock_player = Mock()
-        mock_player.get_score.return_value = 100
-        match = Match(mock_player)
-        result = match.result()
-        self.assertEqual(mock_player.get_score.call_count, 2)
-        # self.assertFalse(mock_player.result())
-        # print(mock_player.result())
+        with patch('entity.Player.score', new_callable=PropertyMock) as obj_mock:
+            obj_mock.return_value = 100
+            player = Player()
+            match = Match(player)
+            result = match.result()
+            self.assertEqual(obj_mock.call_count, 2)
 
     # mock test the round object
     @patch('builtins.input', return_value='<')
@@ -34,16 +33,13 @@ class MockTestMethods(unittest.TestCase):
         with patch.object(round, '_cardset', new_callable=PropertyMock) as obj_mock:
             # assertFalse for wrong 
             with redirect_stdout(f):
-                obj_mock.player_card.return_value = Card('10', 'Spades')
-                obj_mock.house_card.return_value = Card('King', 'Hearts')
-                obj_mock.sign.return_value = True
+                obj_mock.get_cards.return_value = (Card('10', 'Spades'), Card('King', 'Hearts'), True)
                 round.start()
                 self.assertEqual("Starting round #1...\n"\
                     "The House's card is King Hearts\n"\
-                    "Your card is 10 Spades\n"\
-                    "Sorry, you've lost this match!\n", f.getvalue())
+                    "Anonymous's card is 10 Spades\n"\
+                    "Sorry, Anonymous lost this round :(\n", f.getvalue())
             
-
 if __name__ == "__main__":
     unittest.main()
     # print(sys.path)
