a8e041f041d65a2c3be3beef7cdcd2ba71773b89
Minh Phan <anhminh1993@icloud.com>
2022-07-04 16:12:18 +0700

    add -help argparse to navigate players to resources needed

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
+"""
+Phan Vu Anh Minh
+Guessing Game Project
+Copyright 2022
+"""
+import sys
+from os import path
 import logging
 from game import Game
+import argparse
+from config import GAME_DESCRIPTION
+
+PACKAGE_PATH = path.realpath(path.join(path.dirname(__file__), '..'))
+sys.path.append(PACKAGE_PATH)
 
 logger = logging.getLogger(__name__)
+parser = argparse.ArgumentParser(description=GAME_DESCRIPTION)
+parser.parse_args()
 
 def main():
+    """
+    center function to run the game
+    """
     logging.basicConfig(format='%(asctime)s %(levelname)8s' +
                                ' (%(filename)s:%(lineno)s) --- %(message)s',
-                        filename='log/game.log',
+                        filename='../log/game.log',
                         filemode='a',
                         level=logging.INFO)
 
